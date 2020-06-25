bb25e589068c3a938d2f92e5418b0bdd8ee802e9
justHungryMan <justHungryMan@gmail.com>
2020-06-25 15:49:26 +0900

    [CHANGE] Input for Image Dir

--- easy-faster-rcnn.pytorch/infer.py
+++ easy-faster-rcnn.pytorch/infer.py
@@ -2,6 +2,8 @@ import argparse
 import os
 import random
 import torch
+from tqdm import tqdm
+import glob
 
 from PIL import ImageDraw
 from torchvision.transforms import transforms
@@ -13,39 +15,43 @@ from roi.pooler import Pooler
 from config.eval_config import EvalConfig as Config
 
 
-def _infer(path_to_input_image: str, path_to_output_image: str, path_to_checkpoint: str, dataset_name: str, backbone_name: str, prob_thresh: float):
+def _infer(path_to_input_dir: str, path_to_output_dir: str, path_to_checkpoint: str, dataset_name: str, backbone_name: str, prob_thresh: float):
     dataset_class = DatasetBase.from_name(dataset_name)
     backbone = BackboneBase.from_name(backbone_name)(pretrained=False)
     model = Model(backbone, dataset_class.num_classes(), pooler_mode=Config.POOLER_MODE,
                   anchor_ratios=Config.ANCHOR_RATIOS, anchor_sizes=Config.ANCHOR_SIZES,
                   rpn_pre_nms_top_n=Config.RPN_PRE_NMS_TOP_N, rpn_post_nms_top_n=Config.RPN_POST_NMS_TOP_N).cuda()
     model.load(path_to_checkpoint)
+    images = glob.glob(path_to_input_dir + '/*.jpg')
 
     with torch.no_grad():
-        image = transforms.Image.open(path_to_input_image)
-        image_tensor, scale = dataset_class.preprocess(image, Config.IMAGE_MIN_SIDE, Config.IMAGE_MAX_SIDE)
+        for image in tqdm(images):
+            name = image.split("/")[-1]
 
-        detection_bboxes, detection_classes, detection_probs, _ = \
-            model.eval().forward(image_tensor.unsqueeze(dim=0).cuda())
-        detection_bboxes /= scale
+            image = transforms.Image.open(image).convert("RGB")
+            image_tensor, scale = dataset_class.preprocess(image, Config.IMAGE_MIN_SIDE, Config.IMAGE_MAX_SIDE)
 
-        kept_indices = detection_probs > prob_thresh
-        detection_bboxes = detection_bboxes[kept_indices]
-        detection_classes = detection_classes[kept_indices]
-        detection_probs = detection_probs[kept_indices]
+            detection_bboxes, detection_classes, detection_probs, _ = \
+                model.eval().forward(image_tensor.unsqueeze(dim=0).cuda())
+            detection_bboxes /= scale
 
-        draw = ImageDraw.Draw(image)
+            kept_indices = detection_probs > prob_thresh
+            detection_bboxes = detection_bboxes[kept_indices]
+            detection_classes = detection_classes[kept_indices]
+            detection_probs = detection_probs[kept_indices]
 
-        for bbox, cls, prob in zip(detection_bboxes.tolist(), detection_classes.tolist(), detection_probs.tolist()):
-            color = random.choice(['red', 'green', 'blue', 'yellow', 'purple', 'white'])
-            bbox = BBox(left=bbox[0], top=bbox[1], right=bbox[2], bottom=bbox[3])
-            category = dataset_class.LABEL_TO_CATEGORY_DICT[cls]
+            draw = ImageDraw.Draw(image)
 
-            draw.rectangle(((bbox.left, bbox.top), (bbox.right, bbox.bottom)), outline=color)
-            draw.text((bbox.left, bbox.top), text=f'{category:s} {prob:.3f}', fill=color)
+            for bbox, cls, prob in zip(detection_bboxes.tolist(), detection_classes.tolist(), detection_probs.tolist()):
+                color = random.choice(['red', 'green', 'blue', 'yellow', 'purple', 'white'])
+                bbox = BBox(left=bbox[0], top=bbox[1], right=bbox[2], bottom=bbox[3])
+                category = dataset_class.LABEL_TO_CATEGORY_DICT[cls]
 
-        image.save(path_to_output_image)
-        print(f'Output image is saved to {path_to_output_image}')
+                draw.rectangle(((bbox.left, bbox.top), (bbox.right, bbox.bottom)), outline=color)
+                draw.text((bbox.left, bbox.top), text=f'{category:s} {prob:.3f}', fill=color)
+
+            image.save(path_to_output_dir + name)
+        print(f'Output image is saved to {path_to_output_dir}')
 
 
 if __name__ == '__main__':
@@ -62,18 +68,18 @@ if __name__ == '__main__':
         parser.add_argument('--pooler_mode', type=str, choices=Pooler.OPTIONS, help='default: {.value:s}'.format(Config.POOLER_MODE))
         parser.add_argument('--rpn_pre_nms_top_n', type=int, help='default: {:d}'.format(Config.RPN_PRE_NMS_TOP_N))
         parser.add_argument('--rpn_post_nms_top_n', type=int, help='default: {:d}'.format(Config.RPN_POST_NMS_TOP_N))
-        parser.add_argument('input', type=str, help='path to input image')
-        parser.add_argument('output', type=str, help='path to output result image')
+        parser.add_argument('input', type=str, help='path to input dir')
+        parser.add_argument('output', type=str, help='path to output result dir')
         args = parser.parse_args()
 
-        path_to_input_image = args.input
-        path_to_output_image = args.output
+        path_to_input_dir = args.input
+        path_to_output_dir = args.output
         dataset_name = args.dataset
         backbone_name = args.backbone
         path_to_checkpoint = args.checkpoint
         prob_thresh = args.probability_threshold
 
-        os.makedirs(os.path.join(os.path.curdir, os.path.dirname(path_to_output_image)), exist_ok=True)
+        os.makedirs(os.path.join(os.path.curdir, os.path.dirname(path_to_output_dir)), exist_ok=True)
 
         Config.setup(image_min_side=args.image_min_side, image_max_side=args.image_max_side,
                      anchor_ratios=args.anchor_ratios, anchor_sizes=args.anchor_sizes, pooler_mode=args.pooler_mode,
@@ -84,6 +90,6 @@ if __name__ == '__main__':
             print(f'\t{k} = {v}')
         print(Config.describe())
 
-        _infer(path_to_input_image, path_to_output_image, path_to_checkpoint, dataset_name, backbone_name, prob_thresh)
+        _infer(path_to_input_dir, path_to_output_dir, path_to_checkpoint, dataset_name, backbone_name, prob_thresh)
 
     main()
