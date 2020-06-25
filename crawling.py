d3ce9e188c8a9b231f1038c38df886103779183b
justHungryMan <justHungryMan@gmail.com>
2020-06-25 16:19:58 +0900

    [CHANGE]

--- crawling.py
+++ crawling.py
@@ -14,7 +14,7 @@ args = vars(ap.parse_args())
 # set your Microsoft Cognitive Services API key along with (1) the
 # maximum number of results for a given search and (2) the group size
 # for results (maximum of 50 per request)
-API_KEY = "ccdcda76500c43fca7e1fb5da26c05f8"
+API_KEY = ""
 MAX_RESULTS = 250
 GROUP_SIZE = 20
 
